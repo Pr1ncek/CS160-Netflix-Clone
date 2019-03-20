5ffc6714a004f5411808275a4f77cb233da98fb3
Prince Chaudhary <k.prince1997@gmail.com>
2019-03-19 19:35:52 -0700

    Add files via upload
    Initial setup for backend express server
    
    1. Open this folder in your editor 
    2. run 'npm install' from your terminal
    3. run 'npm start' from your terminal
    3. Go to localhost:5000 in your browser
    4. Add more routes

---
+++ backend server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Database Configuration
+const MONGO_URI = require('./config/keys').MONGO_URI;
+mongoose.connect(MONGO_URI, { useNewUrlParser: true }, err => {
+  if (err) console.error(err);
+  else console.log('MongoDB Connected!');
+});
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Router
+app.get('/', (req, res) => {
+  res.json({ Msg: 'Hello World!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server is live!');
+});
