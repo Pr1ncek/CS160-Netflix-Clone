37067a3b855d8e94f452cb524850dbfa3425d48e
nhunguyen190103 <nhu.n.nguyen@sjsu.edu>
2019-03-24 22:20:02 -0700

    added username, email, password fields and register button

--- front-welcome/src/Register.js
+++ front-welcome/src/Register.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+	submitRegister(e) {}
+
 	render() {
 		return (
 			<div>
 			<h1>REGISTER</h1> 
-			<p>This is the Register page</p>
+				<div>
+					<div>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						name="username"
+						placeholder="Username"/>
+					</div>
+					<div>
+					<label htmlFor="email">Email</label>
+					<input
+						type="text"
+						name="email"
+						placeholder="Email"/>
+					</div>
+					<div>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"/>
+					</div>
+					<button
+						type="button"
+						onClick={this
+							.submitRegister.bind(this)}>Create Account</button>
+				</div>
 			</div>
 			);
 	}
