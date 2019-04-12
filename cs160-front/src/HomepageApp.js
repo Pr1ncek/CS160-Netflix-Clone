7c8ff6595ade4f22147b4b0402466cda71259151
Prince K <k.prince1997@gmail.com>
2019-04-12 14:46:09 -0700

    Added logic for fetching top 10 movies in App.js

--- cs160-front/src/HomepageApp.js
+++ cs160-front/src/HomepageApp.js
@@ -1,42 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 //import './HomepageApp.css';
-import HeaderComponent from './HeaderComponent'
-import ProfileIconComponent from './ProfileIconComponent'
-import SearchBarComponent from './SearchBarComponent'
-import VideoBarComponent from './VideoBarComponent'
-import Avatar from './images/Avatar.jpg'
+import HeaderComponent from './HeaderComponent';
+import ProfileIconComponent from './ProfileIconComponent';
+import SearchBarComponent from './SearchBarComponent';
+import VideoBarComponent from './VideoBarComponent';
+import Avatar from './images/Avatar.jpg';
 
 class HomepageApp extends Component {
   render() {
-    return <div/>;
+    return <div />;
   }
-    
-    componentDidMount(){
-        var movies = ["Avatar ", "Spectre", "Pirates"];
-        
-document.getElementById("0").innerHTML = movies[0];
-document.getElementById("1").innerHTML = movies[1];
-document.getElementById("2").innerHTML = movies[2];
-    }
-    
-     render() {
+
+  componentDidMount() {
+    var movies = ['Avatar ', 'Spectre', 'Pirates'];
+    document.getElementById('0').innerHTML = movies[0];
+    document.getElementById('1').innerHTML = movies[1];
+    document.getElementById('2').innerHTML = movies[2];
+  }
+
+  render() {
     return (
       <div>
-        <HeaderComponent /> 
+        <HeaderComponent />
         <ProfileIconComponent />
         <SearchBarComponent />
         <VideoBarComponent />
-       </div>
+      </div>
     );
   }
-    
-    
-    
-    
-    
 }
 
 export default HomepageApp;
-
-       
