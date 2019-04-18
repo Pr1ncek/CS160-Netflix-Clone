4b8c1356bcb1ff7e018eceef706ee973e67397b1
chrisakatibs <chris.thibodeau11@gmail.com>
2019-04-17 23:15:21 -0700

    Fixed objectID access and formatting

--- backend-server/Model/Comment.js
+++ backend-server/Model/Comment.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 
 // Create Schema
 const commentSchema = new mongoose.Schema({
-  movie: { type: Schema.Types.ObjectId, ref: 'movie' },
-  user: { type: Schema.Types.ObjectId, ref: 'user' },
-  text: { type: String },
-  updated: { type: Date, default: Date.now }
+  movie: {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'movie'
+  },
+  user: {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'user'
+  },
+  text: {type: String},
+  updated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
