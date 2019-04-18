e608b0bfb300ea713464fa0b99e1908158e0c044
Prince K <k.prince1997@gmail.com>
2019-04-18 14:21:00 -0700

    Set up homepage on the front end

--- backend-server/Model/Comment.js
+++ backend-server/Model/Comment.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 
-// Create Schema
-const commentSchema = new mongoose.Schema({
-  movie: { type: Schema.Types.ObjectId, ref: 'movie' },
-  user: { type: Schema.Types.ObjectId, ref: 'user' },
-  text: { type: String },
-  updated: { type: Date, default: Date.now }
-});
+
 
 const Comment = mongoose.model('Comment', commentSchema);
 
