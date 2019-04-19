6024cc41552330d6c4beeec19af7f8888d194ed8
Hunter Lai <huntlai123@gmail.com>
2019-04-18 18:44:49 -0700

    reestablished files

---
+++ cs160-front/src/components/MoviePage/CommentComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./CommentComponent.css"
+
+class CommentComponent extends Component {
+	render() {
+		return (
+			<div class = "text-white">
+				< br/>
+				Author
+				< br/>
+				This is an example comment from a person
+				< br/>
+			</div>
+		);
+	}
+}
+
+export default CommentComponent
