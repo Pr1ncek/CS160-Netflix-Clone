42c102dfa7bc3fd0e49e83804a6d1c8bf83fa680
Hunter Lai <huntlai123@gmail.com>
2019-04-18 19:07:04 -0700

    new route for movie

--- cs160-front/src/components/MoviePage/CommentPostComponent.js
+++ cs160-front/src/components/MoviePage/CommentPostComponent.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 // import "./CommentPostComponent.css"
-import { Form, Button } from 'react-bootstrap/'
+//import { Form, Button } from 'react-bootstrap/'
 // import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio} from 'react-bootstrap';
 
 class CommentPostComponent extends Component {
 	render() {
 		return (
 			<div>
-				<Form>
-					<Form.Control as="textarea"/>
-					<Button variant="primary" size="lg">Light</Button>
-				</Form>
 			</div>
 		);
 	}
