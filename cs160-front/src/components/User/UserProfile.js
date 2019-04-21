fb236a8818b547d75c46e1f79de616ab1f53f265
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-20 22:57:36 -0700

    updated userprofile

--- cs160-front/src/components/User/UserProfile.js
+++ cs160-front/src/components/User/UserProfile.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class UserProfile extends React.Component {
   render() {
-    const { isAuthenticated, currentUser} = this.props;
+    const { currentUser} = this.props;
     return (
       <div className="UserProfile mt-5 pt-5">
         <div className="container">
@@ -10,27 +10,24 @@ class UserProfile extends React.Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-8 text-center">User Profile</h1>
               <p className="lead text-center mb-4">Your Notflix Account</p>
-              <div className="row">
-                <div class="col-md-4">
-                  <img src={currentUser.avatar} class="img-fluid"></img>
+              <div className="row border rounded">
+                <div class="col-md-2"></div>
+                <div class="col-md-3">
+                  <img src={currentUser.avatar} class="img-fluid mt-4"></img>
+                  <a href="/EditProfile" class="nav-link ml-4">Edit Profile</a>
                 </div>
-                <div class="col-md-8">
-                  <ul className="lead mb-4">Name: {currentUser.firstName} {currentUser.lastName}</ul>
+                <div class="col-md">
+                  <ul className="lead mt-4 mb-4">Name: {currentUser.firstName} {currentUser.lastName}</ul>
                   <ul className="lead mb-4">Email: {currentUser.email}</ul>  
                 </div>
               </div>
-              <div class="container text-left ml-4">
-                <a href="/EditProfile" class="nav-link">Edit Profile</a>
-              </div>             
-              <nav>
-                <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                  <a class="nav-item nav-link active" id="nav-hist-tab" data-toggle="tab" href="#nav-hist" role="tab" aria-controls="nav-hist" aria-selected="true">History</a>
-                  <a class="nav-item nav-link" id="nav-coms-tab" data-toggle="tab" href="#nav-coms" role="tab" aria-controls="nav-coms" aria-selected="false">Comments</a>
+              <div className="row">
+                <div class="col-md-6 border rounded">
+                  <p className="lead text-center mb-4">History</p>
                 </div>
-              </nav>
-              <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-hist" role="tabpanel" aria-labelledby="nav-hist-tab">...</div>
-                <div class="tab-pane fade" id="nav-coms" role="tabpanel" aria-labelledby="nav-coms-tab">...</div>
+                <div class="col-md-6 border rounded">
+                  <p className="lead text-center mb-4">Comments</p>
+                </div>
               </div>
             </div>
           </div>
