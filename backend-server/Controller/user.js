7f2b98365ab70ee789d19e4bce9170a3099c0561
Sai Sykam <saisykam@Sais-MacBook-Pro.local>
2019-04-22 18:29:24 -0700

    added route to update user info

--- backend-server/Controller/user.js
+++ backend-server/Controller/user.js
@@ -2,3 +2,18 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Model/User');
 
+router.put("/:id/update", (req, res) => {
+    var conditions = {_id: req.params.id};
+
+    User.update(conditions, req.body).then(result => {
+        if(!result){
+            return res.status(404).send("User not found!");
+        }
+        return res.status(200).send("User Information has been updated!");
+    }).catch(err => {
+        console.log(err);
+        return res.status(500);
+    });
+});
+
+module.exports = router;
