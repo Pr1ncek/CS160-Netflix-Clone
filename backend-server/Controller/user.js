06e180c45e06f3500f550c3564aeabbd618c554f
Sai Sykam <saisykam@Sais-MacBook-Pro.local>
2019-04-23 19:12:23 -0700

    fixed bugs in update route after pulling from master

--- backend-server/Controller/user.js
+++ backend-server/Controller/user.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const User = require('../Model/User');
 
 router.put("/:id/update", (req, res) => {
-    var conditions = {_id: req.params.id};
-
-    User.update(conditions, req.body).then(result => {
-        if(!result){
+	var conditions = {_id: req.params.id};
+	User.update(conditions, req.body).then(result => {
+		if(!result){
             return res.status(404).send("User not found!");
         }
         return res.status(200).send("User Information has been updated!");
-    }).catch(err => {
-        console.log(err);
-        return res.status(500);
-    });
+	}).catch(err => {
+		console.log(err);
+		return res.status(500);
+	});
+});
 
 // @route PUT api/users/saveToHistory
 // @desc save a movie to the user's history
@@ -81,5 +81,4 @@ router.delete("/removeFromFavorites", (req, res) => {
 		return res.status(401).send(err);
 	});
 });
-
 module.exports = router;
