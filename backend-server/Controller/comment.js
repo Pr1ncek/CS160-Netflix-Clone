a5ce863a29f29a46f8d6e1eb5df245fb86438072
Haitao Huang <hht1996@hotmail.com>
2019-04-24 21:04:52 -0700

    grab comments with user id

--- backend-server/Controller/comment.js
+++ backend-server/Controller/comment.js
@@ -4,11 +4,28 @@ const router = express.Router();
 const Comment = require('../Model/Comment');
 
 // @route GET api/comments/getMovieComments
-// @desc search for comments by its movieId, grab user's name
+// @desc search for comments by its movieId, grab comments with authors' names,
+//       emails, avatar
 // @access Public
 router.get('/getMovieComments', (req, res) => {
-   Comment.find({movie: req.body.id}).populate('user')
-   .then(comments => {
+  Comment.find({movie: req.body.id})
+    .populate('user', 'firstName lastName email avatar')
+    .then(comments => {
+       return res.status(200).json(comments);
+   })
+   .catch(err => {
+       console.log(err);
+       return res.status(401).send(err);
+   });
+});
+
+// @route GET api/comments/getUserComments
+// @desc search for comments by its movieId, grab comments with movie titles
+// @access Public
+router.get('/getUserComments', (req, res) => {
+  Comment.find({user: req.body.id})
+    .populate('movie', 'title')
+    .then(comments => {
        return res.status(200).json(comments);
    })
    .catch(err => {
