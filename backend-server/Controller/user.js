4044696fb091f5f16f4cbf4e34a499c95ffbd148
Sai Sykam <saisykam@Sais-MacBook-Pro.local>
2019-04-25 17:51:54 -0700

    added route to update user info

--- backend-server/Controller/user.js
+++ backend-server/Controller/user.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Model/User');
 
+// @route PUT api/users/:id/update
+// @desc updates user information
+// @access Public
+router.put("/:id/update", (req, res) => {
+	var conditions = {_id: req.params.id};
+	User.update(conditions, req.body).then(result => {
+		if(!result){
+            return res.status(404).send("User not found!");
+        }
+        return res.status(200).send("User Information has been updated!");
+	}).catch(err => {
+		console.log(err);
+		return res.status(500);
+	});
+});
 
 // @route PUT api/users/saveToHistory
 // @desc save a movie to the user's history
